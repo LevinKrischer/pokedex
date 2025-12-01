f157d80d2ecf24f35a6d237603dbb5b2deda5771
LevinKrischer <levin@levinkrischer.de>
2025-12-01 22:59:32 +0100

    add category functions and control buttons for switching pokemon in bigger Card View

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const allPokemons = [];
+const pCardDialog = document.getElementById("pInfoDialog");
 let loadedPokemons = 0;
+let currentCategory = "Basic";
+let currentPokemon = 0;
 
 async function getPokemonData() {
-    for (let indexPokemon = 1; indexPokemon <= 20; indexPokemon++) {
-        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${indexPokemon}`);
-        let responseAsJson = await response.json();
-        allPokemons.push(responseAsJson);
-    }
-    renderCards();
+  for (let indexPokemon = 1; indexPokemon <= 20; indexPokemon++) {
+      let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${indexPokemon}`);
+      let responseAsJson = await response.json();
+      allPokemons.push(responseAsJson);
+  }
+  renderCards();
 }
 
 async function loadMorePokemons() {
@@ -19,6 +22,21 @@ async function loadMorePokemons() {
     const responseAsJson = await response.json();
     allPokemons.push(responseAsJson);
   }
+  renderMoreCards();
+}
+
+function renderCards() {
+  const cardContent = document.getElementById('cardOverview');
+  cardContent.innerHTML = "";
+
+  for (let indexCard = 0; indexCard < allPokemons.length; indexCard++) {
+    const pokemon = allPokemons[indexCard];
+    const imgHome = pokemon.sprites.other.home.front_default;
+    const nameCap = capitalize(pokemon.species.name);
+    cardContent.innerHTML += getTemplateCardOverview(indexCard, nameCap, imgHome, pokemon);
+    addTypesToCardOverview(indexCard);
+  }
+  loadedPokemons = allPokemons.length;
 }
 
 function renderMoreCards() {
@@ -34,34 +52,16 @@ function renderMoreCards() {
     addTypesToCardOverview(indexCard);
   }
   loadedPokemons = allPokemons.length;
-  loadMorePokemons();
   checkLoadMoreButton();
 }
 
-function renderCards() {
-    const cardContent = document.getElementById('cardOverview');
-    cardContent.innerHTML = "";
-
-    for (let indexCard = 0; indexCard < allPokemons.length; indexCard++) {
-        const pokemon = allPokemons[indexCard];
-        const imgHome = pokemon.sprites.other.home.front_default;
-
-        const nameCap = capitalize(pokemon.species.name);
-
-        cardContent.innerHTML += getTemplateCardOverview(indexCard, nameCap, imgHome, pokemon);
-        addTypesToCardOverview(indexCard);
-    }
-    loadedPokemons = allPokemons.length;
-    loadMorePokemons();
-}
-
 function addTypesToCardOverview(indexCard) {
-    const cardTypeContent = document.getElementById(`typeOverview${indexCard}`);
+  const cardTypeContent = document.getElementById(`typeOverview${indexCard}`);
 
-    for (let indexTypes = 0; indexTypes < allPokemons[indexCard].types.length; indexTypes++) {
-        const type = allPokemons[indexCard].types[indexTypes].type.name;
-        cardTypeContent.innerHTML += getTemplateCardTypes(indexCard, indexTypes, type)
-    }
+  for (let indexTypes = 0; indexTypes < allPokemons[indexCard].types.length; indexTypes++) {
+    const type = allPokemons[indexCard].types[indexTypes].type.name;
+    cardTypeContent.innerHTML += getTemplateCardTypes(indexCard, indexTypes, type)
+  }
 }
 
 function checkLoadMoreButton() {
@@ -72,21 +72,180 @@ function checkLoadMoreButton() {
   }
 }
 
+function openPokemonInfo(indexCard) {
+  currentPokemon = indexCard;
+  pCardDialog.showModal();
+  pCardDialog.classList.add("opened");
+  pCardDialog.innerHTML = getTemplateInformation();
+  loadBasicInformation(currentPokemon);
+}
+
+function closeDialog() {
+  pCardDialog.close();
+  pCardDialog.classList.remove("opened");
+  currentCategory = "Basic";
+}
+
+function loadBasicInformation() {
+  loadBackgroundColorPCard();
+  loadIdPCard();
+  loadNamePCard();
+  loadTypesPCard();
+  loadImgPCard();
+  loadInformationIntoPCard();
+  setCurrentPageInDialog();
+}
+
+function loadBackgroundColorPCard() {
+  const type = allPokemons[currentPokemon].types[0].type.name
+  document.getElementById('pInfoCard').classList.add(`${type}TypeInfo`)
+}
+
+function loadIdPCard() {
+  const id = currentPokemon + 1;
+  document.getElementById('pIdInfoCard').innerHTML = `# ${id}`;
+}
+
+function loadNamePCard() {
+  const nameCap = capitalize(allPokemons[currentPokemon].species.name);
+  document.getElementById('pNameInfoCard').innerHTML = `${nameCap}`;
+}
+
+function loadTypesPCard() {
+  const pCardTypeContent = document.getElementById(`ttTypeInfoCard`);
+  pCardTypeContent.innerHTML = "";
+
+  for (let indexTypes = 0; indexTypes < allPokemons[currentPokemon].types.length; indexTypes++) {
+    const types = allPokemons[currentPokemon].types[indexTypes].type.name
+    pCardTypeContent.innerHTML += getTemplatePCardTypes(types);
+  }
+}
+
+function loadImgPCard() {
+  const imgHome = allPokemons[currentPokemon].sprites.other.home.front_default;
+  const nameCap = capitalize(allPokemons[currentPokemon].species.name);
+  document.getElementById('pCardImg').innerHTML = getTemplateImgHome(imgHome, nameCap);
+} 
+
+function showStatsInformation() {
+  const infoRowContent = document.getElementById(`infoRowContent`);
+  infoRowContent.innerHTML = "";
+  infoRowContent.innerHTML += getTemplatePCardStats();
+  currentCategory = "Stats";
+  setCategoryOnActive();
+}
+
+function setCurrentPageInDialog() {
+  const currentPage = document.getElementById('currentPage');
+  currentPage.innerHTML = currentPokemon + 1;
+}
+
+function loadInformationIntoPCard() {
+  if (currentCategory === "Basic") {
+    showBasicInformation();
+    loadAbilitiesInformationIntoPCard();
+  } else if (currentCategory === "Stats") {
+    showStatsInformation();
+  } else if (currentCategory === "Attacks") {
+    showAttacksInformation();
+  } else if (currentCategory === "Forms") {
+    showPokemonForms();
+  } else return;
+}
+
+function showAttacksInformation() {
+  const infoRowContent = document.getElementById(`infoRowContent`);
+  infoRowContent.innerHTML = "";
+
+  for (let indexAttacks = 0; indexAttacks < allPokemons[currentPokemon].moves.length; indexAttacks++) {
+    const attack = allPokemons[currentPokemon].moves[indexAttacks].move.name;
+    const level = allPokemons[currentPokemon].moves[indexAttacks].version_group_details[0].level_learned_at;
+    infoRowContent.innerHTML += getTemplatePCardAttacks(attack, level);
+  }
+
+  currentCategory = "Attacks";
+  setCategoryOnActive();
+}
+
+function showPokemonForms() {
+  const shinyImg = allPokemons[currentPokemon].sprites.other.home.front_shiny;
+  const artworkImg = allPokemons[currentPokemon].sprites.other["official-artwork"].front_default;
+  const gifImg = allPokemons[currentPokemon].sprites.versions["generation-v"]["black-white"].animated.front_default;
+  const infoRowContent = document.getElementById(`infoRowContent`);
+  infoRowContent.innerHTML = "";
+  infoRowContent.innerHTML = getTemplatePCardForms(shinyImg, artworkImg, gifImg);
+  currentCategory = "Forms";
+  setCategoryOnActive();
+}
+
+
+function showBasicInformation() {
+  const infoRowContent = document.getElementById(`infoRowContent`);
+  const nameCap = capitalize(allPokemons[currentPokemon].species.name)
+  infoRowContent.innerHTML = "";
+  infoRowContent.innerHTML = getTemplatePCardBasics(nameCap);
+  currentCategory = "Basic";
+  setCategoryOnActive();
+}
+
+function loadAbilitiesInformationIntoPCard() {
+  const pCardAbilitiesContent = document.getElementById(`pokeAbilitiesValueInfoCard`);
+  pCardAbilitiesContent.innerHTML = "";
+
+  for (let indexAbilities = 0; indexAbilities < allPokemons[currentPokemon].abilities.length; indexAbilities++) {
+    const abilities = allPokemons[currentPokemon].abilities[indexAbilities].ability.name;
+    pCardAbilitiesContent.innerHTML += getTemplatePCardAbilities(abilities);
+  }
+}
+
+function setCategoryOnActive() {
+  removeCategoryActive();
+  addCategoryActive();
+}
+
+function removeCategoryActive() {
+  const categories = document.querySelectorAll('.categoryBtn');
+
+  for (let i = 0; i < categories.length; i++) {
+    categories[i].classList.remove('active');
+  }
+}
+
+function addCategoryActive() {
+
+  if (currentCategory === "Basic") {
+    document.getElementById('categoryBtnBasicInfoCard').classList.add('active');
+  } else if (currentCategory === "Stats") {
+    document.getElementById('categoryBtnBaseStatsInfoCard').classList.add('active');
+  } else if (currentCategory === "Attacks") {
+    document.getElementById('categoryBtnAttacksInfoCard').classList.add('active');
+  } else if (currentCategory === "Forms") {
+    document.getElementById('categoryBtnFormsInfoCard').classList.add('active');
+  };
+}
+
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function openPokemonInfo(indexCard) {
-    const currentPokemon = indexCard;
-    const pCardDialog = document.getElementById('pInfoDialog')
-    pCardDialog.showModal();
-    pCardDialog.classList.add("opened");
+pCardDialog.addEventListener("click", (event) => {
+  if (event.target === pCardDialog) {
+    closeDialog();
+  }
+});
+
+function nextPokemonPCard() {
+  if (currentPokemon < allPokemons.length - 1) {
+    currentPokemon++;
+    loadBasicInformation();
+  }
 }
 
-// function openGalleryFullsize(i) {
-//   currentI = i;
-//   const galleryFullsize = document.getElementById("galleryFullsize");
-//   galleryFullsize.innerHTML = galleryFullsizeContent(i);
-//   galleryFullsize.showModal();
-//   galleryFullsize.classList.add("opened");
-// }
+function prevPokemonPCard() {
+  if (currentPokemon > 0) {
+    currentPokemon--;
+    loadBasicInformation();
+  }
+}
+
+
--- scripts/templates.js
+++ scripts/templates.js
@@ -20,3 +20,191 @@ function getTemplateCardTypes(indexCard, indexTypes, type) {
                         </div>`;
 }
 
+function getTemplateInformation(){
+    return `
+            <section class="card pInfo" id="pInfoCard">
+              <section class="pinfoHeader">
+                <div class="pId">
+                  <p id="pIdInfoCard" class="t-center"></p>
+                </div>
+                <h2 class="pInfoHeadline" id="pNameInfoCard"></h2>
+                <div class="pType" id="ttTypeInfoCard">
+    
+                </div>
+              </section>
+              <section class="pCardImg" id="pCardImg">
+              </section>
+              <hr />
+              <section class="categoryInfoSection">
+                <section class="categorySelector">
+                  <button onclick="showBasicInformation()" id="categoryBtnBasicInfoCard" class="categoryBtn active" autofocus>
+                    Basic Info
+                  </button>
+                  <button onclick="showStatsInformation()" id="categoryBtnBaseStatsInfoCard" class="categoryBtn">
+                    Base Stats
+                  </button>
+                  <button onclick="showAttacksInformation()" id="categoryBtnAttacksInfoCard" class="categoryBtn" >
+                    Attacks
+                  </button>
+                  <button onclick="showPokemonForms()" id="categoryBtnFormsInfoCard" class="categoryBtn">
+                    Forms
+                  </button>
+                </section>
+                <section class="card categoryInfobox">
+                  <div class="infoRowContentBox" id="infoRowContent">
+                    </div>
+                  </div>
+                </section>
+              </section>
+              <section class="pageSelector">
+                <button
+                  id="pageSelectorBtnPrev"
+                  class="pageSelectorBtnPrev"
+                  alt=""
+                  onclick="prevPokemonPCard()"
+                ></button>
+                <div class="pageCurrent">
+                  <b><span class="f-white" id="currentPage">${currentPokemon + 1}</span> of
+                  <span class="f-white" id="totalPokemonsLoaded">${allPokemons.length}</b><br>loaded Pokémon</span>
+                </div>
+                <button
+                  id="pageSelectorBtnNext"
+                  class="pageSelectorBtnNext"
+                  alt=""
+                  onclick="nextPokemonPCard()"
+                ></button>
+              </section>
+            </section>
+`
+}
+
+function getTemplateImgHome(imgHome, nameCap) {
+  return `
+      <img
+        src="${imgHome}"
+        alt="Picture of ${nameCap}"
+      />
+      `;
+}
+
+function getTemplatePCardTypes(types) {
+    return `
+            <div class="tooltip pCardTypes" >
+                <img
+                    src="./assets/icons/types/${types}.png"
+                    alt="Icon of type ${types}"
+                    class="type"
+                />
+                <span class="tooltiptext bg-${types}">${types}</span>
+                </div>
+                `;
+}
+
+function getTemplatePCardBasics(nameCap) {
+    return `
+        <div class="infoRow">
+          <div id="pokeHeightKeyInfoCard" class="basicInfoLeft">
+            Height:
+          </div>
+          <div id="pokeHeightValueInfoCard" class="basicInfoRight">
+            ${allPokemons[currentPokemon].height}
+          </div>
+        </div>
+        <div class="infoRow">
+          <div id="pokeWeightKeyInfoCard" class="basicInfoLeft">
+            Weight:
+          </div>
+          <div id="pokeWeightValueInfoCard" class="basicInfoRight">
+            ${allPokemons[currentPokemon].weight}
+          </div>
+        </div>
+        <div class="infoRow">
+          <div id="pokeSpeciesKeyInfoCard" class="basicInfoLeft">
+            Species:
+          </div>
+          <div id="pokeSpeciesValueInfoCard" class="basicInfoRight">
+            ${nameCap}
+          </div>
+        </div>
+        <div class="infoRow">
+          <div id="pokeAbilitiesKeyInfoCard" class="basicInfoLeft">
+            Abilities:
+          </div>
+          <div id="pokeAbilitiesValueInfoCard" class="basicInfoRight pl-16">
+          </div>
+        </div>
+    `;
+}
+
+function getTemplatePCardAbilities(abilities) {
+    return `
+        <li>${abilities}</li>
+    `;
+}
+
+function getTemplatePCardStats() {
+    return `
+        <div class="infoRow">
+            <div class="basicInfoLeft">hp:</div>
+            <div class="basicInfoRight progressBar">
+            <div class="progressBarInner" style="width:${allPokemons[currentPokemon].stats[0].base_stat}%">${allPokemons[currentPokemon].stats[0].base_stat}</div>
+            </div>
+        </div>
+        <div class="infoRow">
+            <div class="basicInfoLeft">attack:</div>
+            <div class="basicInfoRight progressBar">
+            <div class="progressBarInner" style="width:${allPokemons[currentPokemon].stats[1].base_stat}%">${allPokemons[currentPokemon].stats[1].base_stat}</div>
+            </div>
+        </div>
+        <div class="infoRow">
+            <div class="basicInfoLeft">defense:</div>
+            <div class="basicInfoRight progressBar">
+            <div class="progressBarInner" style="width:${allPokemons[currentPokemon].stats[2].base_stat}%">${allPokemons[currentPokemon].stats[2].base_stat}</div>
+            </div>
+        </div>
+        <div class="infoRow">
+            <div class="basicInfoLeft">special attack:</div>
+            <div class="basicInfoRight progressBar">
+            <div class="progressBarInner" style="width:${allPokemons[currentPokemon].stats[3].base_stat}%">${allPokemons[currentPokemon].stats[3].base_stat}</div>
+            </div>
+        </div>
+        <div class="infoRow">
+            <div class="basicInfoLeft">special defense:</div>
+            <div class="basicInfoRight progressBar">
+            <div class="progressBarInner" style="width:${allPokemons[currentPokemon].stats[4].base_stat}%">${allPokemons[currentPokemon].stats[4].base_stat}</div>
+            </div>
+        </div>
+        <div class="infoRow">
+            <div class="basicInfoLeft">speed:</div>
+            <div class="basicInfoRight progressBar">
+            <div class="progressBarInner" style="width:${allPokemons[currentPokemon].stats[5].base_stat}%">${allPokemons[currentPokemon].stats[5].base_stat}</div>
+            </div>
+        </div>
+        `;
+}
+
+function getTemplatePCardAttacks(attack, level) {
+  return `
+      <div class="infoRow">
+          <div class="basicInfoLeft">${attack}</div>
+          <div class="basicInfoRight">Level ${level}</div>
+      </div>
+  `;
+}
+
+function getTemplatePCardForms(shinyImg, artworkImg, gifImg) {
+  return ` 
+      <div class="infoRow pCardForm">
+        <div class="basicInfoLeft"><p>Shiny</p></div>
+        <div class="basicInfoRight"><img src="${shinyImg}" alt="Shiny Sprite"></div>
+      </div>
+      <div class="infoRow pCardForm">
+        <div class="basicInfoLeft"><p>Artwork</p></div>
+        <div class="basicInfoRight"><img src="${artworkImg}" alt="Official Artwork"></div>
+      </div>
+      <div class="infoRow pCardForm">
+        <div class="basicInfoLeft"><p>Animated</p></div>
+        <div class="basicInfoRight"><img src="${gifImg}" alt="Official Artwork"></div>
+      </div>
+      `;
+}
